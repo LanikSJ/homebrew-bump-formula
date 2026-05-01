dadf1194536766cf79b0550dfa0e7eb5c13319f4
LanikSJ <LanikSJ@users.noreply.github.com>
2026-04-30 20:19:59 -0700

    fix: handle authentication errors during user lookup, update footer link, and restrict workflow runs to authorized environments (#38)
    - handle authentication errors during user lookup
    - update footer link
    - restrict workflow runs to authorized environments

--- main.rb
+++ main.rb
@@ -65,20 +65,29 @@ module Homebrew
   # Application tokens don't work with GitHub API's `/user` endpoit.
   if user_name.blank? && user_email.blank?
     # Get user details
-    user = GitHub::API.open_rest "#{GitHub::API_URL}/user"
-    user_id = user["id"]
-    user_login = user["login"]
-    user_name = user["name"] || user["login"] if user_name.blank?
-    if user_email.blank?
-      user_email = user["email"] || (
-        # https://help.github.com/en/github/setting-up-and-managing-your-github-user-account/setting-your-commit-email-address
-        user_created_at = Date.parse user["created_at"]
-        plus_after_date = Date.parse "2017-07-18"
-        need_plus_email = (user_created_at - plus_after_date).positive?
-        user_email = "#{user_login}@users.noreply.github.com"
-        user_email = "#{user_id}+#{user_email}" if need_plus_email
-        user_email
-      )
+    begin
+      user = GitHub::API.open_rest "#{GitHub::API_URL}/user"
+      user_id = user["id"]
+      user_login = user["login"]
+      user_name = user["name"] || user["login"] if user_name.blank?
+      if user_email.blank?
+        user_email = user["email"] || (
+          # https://help.github.com/en/github/setting-up-and-managing-your-github-user-account/setting-your-commit-email-address
+          user_created_at = Date.parse user["created_at"]
+          plus_after_date = Date.parse "2017-07-18"
+          need_plus_email = (user_created_at - plus_after_date).positive?
+          user_email = "#{user_login}@users.noreply.github.com"
+          user_email = "#{user_id}+#{user_email}" if need_plus_email
+          user_email
+        )
+      end
+    rescue GitHub::API::AuthenticationFailedError
+      odie <<~EOS
+        GitHub API Error: Bad credentials.
+        This usually means your 'token' is invalid or expired.
+        If you are using the default GITHUB_TOKEN, please note that it does not work with the GitHub API's '/user' endpoint.
+        You can avoid this by providing 'user_name' and 'user_email' inputs to the Action.
+      EOS
     end
   end
 
@@ -99,7 +108,7 @@ module Homebrew
   else
     message + "\n\n"
   end
-  message += "[`action-homebrew-bump-formula`](https://github.com/LanikSJ/action-homebrew-bump-formula)"
+  message += "[`Homebrew Bump Formula`](https://github.com/LanikSJ/homebrew-bump-formula)"
 
   unless force.false?
     brew_repo = read_brew "--repository"
