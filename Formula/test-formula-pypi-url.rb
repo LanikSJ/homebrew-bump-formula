7db6cb7881ad405e49701396447339e87b99341a
LanikSJ <laniksj@gmail.com>
2026-05-01 00:25:17 -0700

    feat: add test-formula-pypi-url formula and update CI workflow test suite

---
+++ Formula/test-formula-pypi-url.rb
@@ -0,0 +1,19 @@
+class TestFormulaPypiUrl < Formula
+  desc "Formula to test Action"
+  homepage "https://github.com/AlDanial/cloc"
+  url "https://github.com/AlDanial/cloc/archive/v1.0.tar.gz"
+  sha256 "1b4567ed50555e10920e5bd804a6a4e2c42ec70bb74f14a83f2680fe9eaf9727"
+  license "MIT"
+
+  def install
+    (buildpath / "test").write <<~EOS
+      test
+    EOS
+
+    share.install "test"
+  end
+
+  test do
+    sleep 1
+  end
+end
