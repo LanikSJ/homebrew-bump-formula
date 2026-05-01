eb09652d804e4e292483963f523ffffdb4a3fb6d
LanikSJ <laniksj@gmail.com>
2026-05-01 00:19:10 -0700

    chore: update test formula source to cloc and increment version tag

--- Formula/test-formula-pypi-url.rb
+++ Formula/test-formula-pypi-url.rb
@@ -1,10 +1,9 @@
 class TestFormulaPypiUrl < Formula
   desc "Formula to test Action"
-  homepage "https://commitizen-tools.github.io/commitizen/"
-  url "https://github.com/commitizen-tools/commitizen/archive/v1.0.0.tar.gz"
+  homepage "https://github.com/AlDanial/cloc"
+  url "https://github.com/AlDanial/cloc/archive/v1.0.tar.gz"
   sha256 "1b4567ed50555e10920e5bd804a6a4e2c42ec70bb74f14a83f2680fe9eaf9727"
   license "MIT"
-  head "https://github.com/commitizen-tools/commitizen.git"
 
   def install
     (buildpath / "test").write <<~EOS
