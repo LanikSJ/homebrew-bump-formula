6df61cbed4cc4e6f7ef498fc232be610dac0bc63
LanikSJ <LanikSJ@users.noreply.github.com>
2026-09-13 09:41:24 +0000

    test-formula-url 7

--- Formula/test-formula-url.rb
+++ Formula/test-formula-url.rb
@@ -1,8 +1,8 @@
 class TestFormulaUrl < Formula
   desc "Formula to test Action"
   homepage "https://github.com/dawidd6/action-homebrew-bump-formula"
-  url "https://github.com/dawidd6/action-homebrew-bump-formula/archive/v1.tar.gz"
-  sha256 "1c890d077e56bb5f2192ac079a1c3eec820c431011411a165777ce201e5905b5"
+  url "https://github.com/dawidd6/action-homebrew-bump-formula/archive/v7.tar.gz"
+  sha256 "38a90d077e56bb5f2192ac079a1c3eec820c431011411a165777ce201e5905b5"
   license "MIT"
 
   def install
