f9417ab9300f6df673d5d255d9d0294a3b44f8b1
LanikSJ <LanikSJ@users.noreply.github.com>
2026-09-13 09:41:27 +0000

    test-formula-git-revision 0.8.3

--- Formula/test-formula-git-revision.rb
+++ Formula/test-formula-git-revision.rb
@@ -2,8 +2,8 @@ class TestFormulaGitRevision < Formula
   desc "Formula to test Action"
   homepage "https://github.com/Debian/dh-make-golang"
   url "https://github.com/Debian/dh-make-golang.git",
-    tag: "v0.1.0",
-    revision: "98e6701337d2848541192f973d74851a22b272a1"
+    tag: "v0.8.3",
+    revision: "ecea0ce570d18808399ee16515e30a955707eaa8"
   license "MIT"
   head "https://github.com/Debian/dh-make-golang.git"
 
